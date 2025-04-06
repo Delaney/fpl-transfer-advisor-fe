1dfcbde1ed519cf36eafce7b6f36a8e958861ae5
Delaney Sylvans <delaneysylvans@gmail.com>
2025-04-06 19:02:25 +0100

    update recommendations URL

--- src/app/components/RecommendationForm.tsx
+++ src/app/components/RecommendationForm.tsx
@@ -29,7 +29,7 @@ export default function RecommendationForm() {
         setError(null);
         clearRecommendations();
         try {
-            const response = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + `simple-analysis-${mode}`, {
+            const response = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + `simple-analysis/${mode}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
